c92bc085f6be4834a192e85b8f7ae1b08835153e
Dan Baissa <71029346+DanBaissa@users.noreply.github.com>
2026-05-03 20:06:30 -0400

    Add bad-donor regimes to SC benchmark design

--- bindings/r/robustcause/R/sc_benchmark_design.R
+++ bindings/r/robustcause/R/sc_benchmark_design.R
@@ -1,4 +1,4 @@
-make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spike", "donor_pre_spike", "treated_and_donor_pre_spike", "heavy_tails", "donor_pool_mismatch", "predictor_spike", "mixed_outcome_predictor"),
+make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spike", "donor_pre_spike", "treated_and_donor_pre_spike", "heavy_tails", "donor_pool_mismatch", "predictor_spike", "mixed_outcome_predictor", "bad_donor_pre_path", "bad_donor_predictors", "bad_donor_mixed"),
                                      n_donors = c(10L, 25L),
                                      n_pre = c(15L, 30L, 60L),
                                      n_post = 10L,
@@ -13,7 +13,8 @@ make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spik
                                      n_predictors = c(0L, 8L),
                                      predictor_noise_sd = 0.25,
                                      predictor_contamination_rate = c(0, 0.10),
-                                     predictor_contamination_magnitude = c(4, 8)) {
+                                     predictor_contamination_magnitude = c(4, 8),
+                                     bad_donor_rate = c(0.10, 0.20)) {
   design <- expand.grid(
     contamination = contamination,
     n_donors = n_donors,
@@ -31,18 +32,21 @@ make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spik
     predictor_noise_sd = predictor_noise_sd,
     predictor_contamination_rate = predictor_contamination_rate,
     predictor_contamination_magnitude = predictor_contamination_magnitude,
+    bad_donor_rate = bad_donor_rate,
     KEEP.OUT.ATTRS = FALSE,
     stringsAsFactors = FALSE
   )
 
   min_contam_mag <- contamination_magnitude[[1]]
   min_pred_contam_mag <- predictor_contamination_magnitude[[1]]
+  min_bad_donor_rate <- bad_donor_rate[[1]]
 
   keep_none <- design$contamination == "none" &
     design$contamination_rate == 0 &
     design$predictor_contamination_rate == 0 &
     design$contamination_magnitude == min_contam_mag &
     design$predictor_contamination_magnitude == min_pred_contam_mag &
+    design$bad_donor_rate == min_bad_donor_rate &
     design$donor_mismatch_strength == 0
 
   keep_mismatch <- design$contamination == "donor_pool_mismatch" &
@@ -50,11 +54,13 @@ make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spik
     design$predictor_contamination_rate == 0 &
     design$contamination_magnitude == min_contam_mag &
     design$predictor_contamination_magnitude == min_pred_contam_mag &
+    design$bad_donor_rate == min_bad_donor_rate &
     design$donor_mismatch_strength > 0
 
   keep_outcome <- design$contamination %in% c("treated_pre_spike", "donor_pre_spike", "treated_and_donor_pre_spike", "heavy_tails") &
     design$contamination_rate > 0 &
     design$predictor_contamination_rate == 0 &
+    design$bad_donor_rate == min_bad_donor_rate &
     design$donor_mismatch_strength == 0
 
   keep_predictor <- design$contamination == "predictor_spike" &
@@ -70,7 +76,37 @@ make_sc_benchmark_design <- function(contamination = c("none", "treated_pre_spik
     design$predictor_contamination_rate > 0 &
     design$donor_mismatch_strength == 0
 
-  design <- design[keep_none | keep_mismatch | keep_outcome | keep_predictor | keep_mixed, , drop = FALSE]
+  keep_bad_donor_pre <- design$contamination == "bad_donor_pre_path" &
+    design$contamination_rate == 0 &
+    design$predictor_contamination_rate == 0 &
+    design$predictor_contamination_magnitude == min_pred_contam_mag &
+    design$donor_mismatch_strength == 0
+
+  keep_bad_donor_predictors <- design$contamination == "bad_donor_predictors" &
+    design$n_predictors > 0 &
+    design$contamination_rate == 0 &
+    design$predictor_contamination_rate == 0 &
+    design$contamination_magnitude == min_contam_mag &
+    design$donor_mismatch_strength == 0
+
+  keep_bad_donor_mixed <- design$contamination == "bad_donor_mixed" &
+    design$n_predictors > 0 &
+    design$contamination_rate == 0 &
+    design$predictor_contamination_rate == 0 &
+    design$donor_mismatch_strength == 0
+
+  design <- design[
+    keep_none |
+      keep_mismatch |
+      keep_outcome |
+      keep_predictor |
+      keep_mixed |
+      keep_bad_donor_pre |
+      keep_bad_donor_predictors |
+      keep_bad_donor_mixed,
+    ,
+    drop = FALSE
+  ]
   rownames(design) <- NULL
   design$design_id <- seq_len(nrow(design))
   design
